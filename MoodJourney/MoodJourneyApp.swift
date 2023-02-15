9b9570253ac5dd063ef1af014a3baee3b480cbe4
keyvan <keyvan.ygh@gmail.com>
2023-02-15 10:22:05 -0500

    init

---
+++ MoodJourney/MoodJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodJourneyApp.swift
+//  MoodJourney
+//
+//  Created by sh on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
