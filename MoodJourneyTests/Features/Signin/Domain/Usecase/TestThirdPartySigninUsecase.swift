68c42f2aea01ed164587fbd856e9b497803821a0
keyvan <keyvan.ygh@gmail.com>
2023-02-15 21:07:01 -0500

    3rd party sign in test added

---
+++ MoodJourneyTests/Features/Signin/Domain/Usecase/TestThirdPartySigninUsecase.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdPartySigninUsecase.swift
+//  MoodJourneyTests
+//
+//  Created by Keyvan on 2/15/23.
+//
+
+import XCTest
+
+final class TestThirdPartySigninUsecase: XCTestCase {
+    
+    func test_reciveUserEntity_whenAuthIsSuccess() {
+        let sut = sut(repository: mockedRepostiry)
+        let result = sut.execute(
+            userID : "String",
+            hasImage : false,
+            name : "name",
+            family : "fname",
+            givenName : "gname",
+            imageURL : URL(string: "string"))
+        switch(result){
+        case .success(let userEntity):
+            XCTAssertNotNil(userEntity)
+        default:
+            XCTFail()
+        }
+    }
+    
+    // MARK: Helpers
+    
+    final private func sut(repository : ISigninRepository) -> ThirdPartySigninUsecase {
+        return ThirdPartySigninUsecase(repository : ISigninRepository)
+    }
+    
+}
