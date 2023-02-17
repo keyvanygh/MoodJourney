c0c1818674480d5eaabf936539ac217728684206
keyvan <keyvan.ygh@gmail.com>
2023-02-16 20:54:15 -0500

    google config

--- MoodJourney/MoodJourneyApp.swift
+++ MoodJourney/MoodJourneyApp.swift
@@ -6,12 +6,16 @@
 
 
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct MoodJourneyApp: App {
     var body: some Scene {
         WindowGroup {
             SigninView()
+                .onOpenURL { url in
+                  GIDSignIn.sharedInstance.handle(url)
+                }
         }
     }
 }
