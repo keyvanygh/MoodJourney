6a9f4f3839ae48c9feffa51c23f641d9cb444f52
keyvan <keyvan.ygh@gmail.com>
2023-02-19 11:53:08 -0500

    remove todo is done

--- MoodJourney/Core/AnyError.swift
+++ MoodJourney/Core/AnyError.swift
@@ -10,11 +10,3 @@ import Foundation
 enum AnyError : Error {
     case error
 }
-
-
-
-// TODO:
-/// 1 - change all entitiy objects to entity+extension
-/// 2 - change coredata entitiys names
-/// 3 - change repository to load form local datasource and then update from remote data source
-/// 4 - fix tests and write new tests
