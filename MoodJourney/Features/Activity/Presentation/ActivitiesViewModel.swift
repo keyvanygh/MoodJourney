a4945003b295bc3f2592725e30c06c78837439b7
keyvan <keyvan.ygh@gmail.com>
2023-02-18 19:42:40 -0500

    activity ViewModel input output

--- MoodJourney/Features/Activity/Presentation/ActivitiesViewModel.swift
+++ MoodJourney/Features/Activity/Presentation/ActivitiesViewModel.swift
@@ -7,9 +7,34 @@
 
 import Foundation
 
-class ActivitiesViewModel: ObservableObject {
+class ActivitiesViewModel:
+    AnyViewModel,
+    ActivitiesViewModelInput,
+    ActivitiesViewModelOutput {
+    
+    public var inputs: ActivitiesViewModelInput { return self }
+    public var outputs: ActivitiesViewModelOutput { return self }
     let fetchActivitiesUsecase : FetchActivitiesUsecase = FetchActivitiesUsecase(repository: ActivityRepositoryImp())
     
+    // MARK: - Outputs
+    @Published var activities: [ActivityEntity] = []
     
-                                                                                 
+    
+    // MARK: - Inputs
+    func fetchActivies() {
+        let result = fetchActivitiesUsecase.execute()
+        switch result{
+        case .success(let result):
+            activities = result
+        case .failure(_):
+            break
+        }
+    }
+}
+protocol ActivitiesViewModelInput{
+    func fetchActivies()
+}
+
+protocol ActivitiesViewModelOutput{
+    var activities: [ActivityEntity] { get }
 }
