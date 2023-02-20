3f5cff7683290825b695127e12c25f6ca6d6a2ea
keyvan <keyvan.ygh@gmail.com>
2023-02-20 17:37:13 -0500

    repository document

--- MoodJourney/Features/Feeling/Domain/Repository/IFeelingRepository.swift
+++ MoodJourney/Features/Feeling/Domain/Repository/IFeelingRepository.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 protocol FeelingRepository {
+
+    /// add feeling to activity with acitivtyID
+    /// - Parameters:
+    ///   - activityID: activity id
+    ///   - feeling: feeling
+    ///   - message: some explanation for the feeling
+    ///   - imageURLString: image url absoluteString
+    /// - Returns:
+    ///   - success: array of FeelingEntitys
+    ///   - fail: FeelingError
     func addFeeling(activityID: String,feeling: Feeling,message: String?, imageURLString: String?) -> Result<Bool,Error>
+    
+    /// fetch feelings from activity with activityID
+    /// - Parameters:
+    ///   - activityID: activity id
+    /// - Returns:
+    ///   - success: array of FeelingEntitys
+    ///   - fail: FeelingError
     func fetchFeelings(fromActivity activityID: String) -> Result<[FeelingEntity],Error>
 }
