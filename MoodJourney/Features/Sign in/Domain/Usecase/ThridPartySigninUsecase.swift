2663e74a4cd1a8230b532d0bbbb838eaafeb50ae
keyvan <keyvan.ygh@gmail.com>
2023-02-20 12:22:50 -0500

    usecase doc

--- MoodJourney/Features/Sign in/Domain/Usecase/ThridPartySigninUsecase.swift
+++ MoodJourney/Features/Sign in/Domain/Usecase/ThridPartySigninUsecase.swift
@@ -7,13 +7,26 @@
 
 import Foundation
 
-class ThirdPartySigninUsecase {
-    let repository : SigninRepository
+/// will execute this usecase when user sign in with 3'rd party providers e.g: Apple,Google
+struct ThirdPartySigninUsecase {
+    fileprivate let repository : SigninRepository
     
     init(repository: SigninRepository) {
         self.repository = repository
     }
     
+    /// execute the usecae
+    /// - Parameters:
+    ///   - thirdParty: 3'rd party type e.g: .Google
+    ///   - userID: userID provided by 3'rd party
+    ///   - name: name provided by 3'rd party
+    ///   - hasImage if user has an image or not
+    ///   - family: last name provided by 3'rd party
+    ///   - givenName: given name provided by 3'rd party
+    ///   - imageURL: user image url
+    /// - Returns:
+    ///   - success: UserEntity
+    ///   - fail: SigninError
     func execute(
         thirdParty : ThirdParty,
         userID : String,
@@ -23,5 +36,5 @@ class ThirdPartySigninUsecase {
         givenName : String?,
         imageURL : URL?) -> Result<UserEntity,Error> {
             return repository.signin(withThirdParty: .Google, userID: userID, hasImage: hasImage, name: name, family: family, givenName: givenName, imageURL: imageURL)
-    }
+        }
 }
