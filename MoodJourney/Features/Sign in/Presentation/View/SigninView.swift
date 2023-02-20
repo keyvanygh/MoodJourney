2c8c468ed68380f688ec7ac43de3202cf2cbe901
keyvan <keyvan.ygh@gmail.com>
2023-02-20 12:26:55 -0500

    SigninView

--- MoodJourney/Features/Sign in/Presentation/View/SigninView.swift
+++ MoodJourney/Features/Sign in/Presentation/View/SigninView.swift
@@ -10,21 +10,19 @@ import SwiftUI
 struct SigninView: View {
     @StateObject var viewModel : SigninViewModel = SigninViewModel()
     @EnvironmentObject private var coordinator:  Coordinator
-    @State var navigateToActvities = false
     var body: some View {
         VStack {
             SigninWithAppleButton(viewModel: viewModel)
                 .frame(height: 44)
                 .padding(.top,188)
                 .padding(.bottom,12)
-            
                 .padding(.horizontal,32)
             SigninWithGoogleButton(viewModel: viewModel)
                 .frame(height: 44)
                 .padding(.bottom,188)
             
         }
-        .background(Color.green)
+        .background(Color.green.ignoresSafeArea())
         .onChange(of: viewModel.user) { user in
             guard let user = viewModel.user else {return}
             coordinator.push(.Activity(user: user))
