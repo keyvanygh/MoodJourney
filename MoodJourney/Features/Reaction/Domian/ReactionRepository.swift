5e4b83ccd347cafb5e23dfb36afa0a1d74e62f36
keyvan <keyvan.ygh@gmail.com>
2023-02-22 10:50:32 -0500

    addReactionToFeelingUsecase added

---
+++ MoodJourney/Features/Reaction/Domian/ReactionRepository.swift
@@ -0,0 +1,8 @@
+//
+//  ReactionRepository.swift
+//  MoodJourney
+//
+//  Created by Keyvan on 2/22/23.
+//
+
+import Foundation
---
+++ MoodJourney/Features/Reaction/Domian/Usecase/AddReactionToFeeling.swift
@@ -0,0 +1,16 @@
+//
+//  AddReactionToFeeling.swift
+//  MoodJourney
+//
+//  Created by Keyvan on 2/22/23.
+//
+
+import Foundation
+
+class AddReactionToFeeling {
+    let repository: ReactionRepository
+    init(repository: ReactionRepository) {
+        self.repository = repository
+    }
+    func execute(){}
+}
