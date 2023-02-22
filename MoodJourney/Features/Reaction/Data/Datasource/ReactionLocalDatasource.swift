d8c18a676d00633ccb36bca5a3e30b20999b441d
keyvan <keyvan.ygh@gmail.com>
2023-02-22 11:32:09 -0500

    add reaction added to datasouce

--- MoodJourney/Features/Reaction/Data/Datasource/ReactionLocalDatasource.swift
+++ MoodJourney/Features/Reaction/Data/Datasource/ReactionLocalDatasource.swift
@@ -14,7 +14,11 @@ class ReactionLocalDatasource {
     func addReaction(
         _ reaction: ReactionEntity,
         to feeling: FeelingEntity) throws -> ReactionEntity {
-        
-            
+            guard let reaction = dbm.add(entity: .Reaction) as? ReactionEntity else {throw(AnyError.error)}
+            reaction.feeling = feeling
+            reaction.gifUrlString = "gifURL.absoluteURL"
+            reaction.emoji = "emoji"
+            return reaction
+//            feeling.addToReactions(reaction)
     }
 }
