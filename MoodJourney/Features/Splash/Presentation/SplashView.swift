1e7aaa374723585bfed3262bb7ef5189fa8efc20
keyvan <keyvan.ygh@gmail.com>
2023-02-21 20:46:50 -0500

    in out added

--- MoodJourney/Features/Splash/Presentation/SplashView.swift
+++ MoodJourney/Features/Splash/Presentation/SplashView.swift
@@ -13,13 +13,13 @@ struct SplashView: View {
     var body: some View {
         Text("Splash")
             .onAppear(){
-                vm.startFlow()
+                vm.inputs.startFlow()
             }
-            .onChange(of: vm.userSignedin) { userSignedin in
+            .onChange(of: vm.outputs.userSignedin) { userSignedin in
                 guard let userSignedin = userSignedin else{return}
                 if !userSignedin {coordinator.push(.Signin)}
                 else{
-                    guard let user = vm.user else{return}
+                    guard let user = vm.outputs.user else{return}
                     coordinator.push(.Activity(user: user))
                 }
             }
