6137c526927277a633b28fd071ac41e4ce16632a
keyvan <keyvan.ygh@gmail.com>
2023-02-22 11:34:35 -0500

    vm added

---
+++ MoodJourney/Features/Reaction/Presentation/ReactionView.swift
@@ -0,0 +1,20 @@
+//
+//  ReactionView.swift
+//  MoodJourney
+//
+//  Created by Keyvan on 2/22/23.
+//
+
+import SwiftUI
+
+struct ReactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReactionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReactionView()
+    }
+}
---
+++ MoodJourney/Features/Reaction/Presentation/ReactionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReactionView.swift
+//  MoodJourney
+//
+//  Created by Keyvan on 2/22/23.
+//
+
+import Foundation
+
+class ReactionViewModel: ObservableObject {
+    
+    
+}
