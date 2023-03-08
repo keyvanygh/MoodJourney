a2aba3d1fb3ccba187d22143f44a693a80338c25
keyvan <keyvan.ygh@gmail.com>
2023-03-08 13:32:55 -0500

    signinWithApple usecase

---
+++ MoodJourney/Features/Sign in/Domain/Usecase/SigninWithApple.swift
@@ -0,0 +1,56 @@
+//
+//  SigninWithGoogle.swift
+//  MoodJourney
+//
+//  Created by Keyvan on 2/15/23.
+//
+
+import Foundation
+
+/// will execute this usecase when user sign in with 3'rd party providers e.g: Apple,Google
+class SigninWithApple: AnyUsecase {
+    var repository: any SigninRepository
+    
+    init(repository: any SigninRepository) {
+        self.repository = repository
+    }
+    
+    /// execute the usecae
+    /// - Parameters:
+    ///   - thirdParty: 3'rd party type e.g: .Google
+    ///   - userID: userID provided by 3'rd party
+    ///   - name: name provided by 3'rd party
+    ///   - hasImage if user has an image or not
+    ///   - family: last name provided by 3'rd party
+    ///   - givenName: given name provided by 3'rd party
+    ///   - imageURL: user image url
+    /// - Returns:
+    ///   - success: UserEntity
+    ///   - fail: SigninError
+    public final func callAsFunction(
+        userID: String,
+        hasImage: Bool? = false,
+        name: String? = "",
+        family: String? = "",
+        givenName: String? = "",
+        imageURL: URL? = nil) -> Result<UserEntity, Error> {
+            
+            do {
+                try validate(userID: userID)
+            } catch {
+                return .failure(error)
+            }
+            
+            return repository.signin(
+                withThirdParty: .apple,
+                userID: userID,
+                hasImage: hasImage,
+                name: name,
+                family: family,
+                givenName: givenName,
+                imageURL: imageURL)
+        }
+    private final func validate(userID: String) throws {
+        if userID.isEmpty {throw(AnyError.error)}
+    }
+}
