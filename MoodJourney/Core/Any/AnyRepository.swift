73f7a3f225da1de44acf160c631c0ad39c489be1
keyvan <keyvan.ygh@gmail.com>
2023-03-07 21:54:16 -0500

    any repository

---
+++ MoodJourney/Core/Any/AnyRepository.swift
@@ -0,0 +1,15 @@
+//
+//  AnyRepository.swift
+//  MoodJourney
+//
+//  Created by Keyvan on 3/7/23.
+//
+
+import Foundation
+
+protocol AnyRepository {
+    associatedtype LocalDatasource
+    associatedtype RemoteDatasource
+    var lds: LocalDatasource {get}
+    var rds: RemoteDatasource {get}
+}
